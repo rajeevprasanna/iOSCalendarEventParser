e7d837695c018637faba6327bb36a98f29a603a1
rajeevprasanna <rajeevprasanna@gmail.com>
2016-02-03 14:52:55 +0530

    configuring spec

---
+++ CalendarManager/CalendarEventParser.h
@@ -0,0 +1,13 @@
+//
+//  CalendarEventParser.h
+//  iOSCalendarEventParser
+//
+//  Created by Rajeev Kumar Kallempudi on 2/3/16.
+//  Copyright © 2016 rajeevprasanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalendarEventParser : NSObject
+
+@end
---
+++ CalendarManager/CalendarEventParser.m
@@ -0,0 +1,13 @@
+//
+//  CalendarEventParser.m
+//  iOSCalendarEventParser
+//
+//  Created by Rajeev Kumar Kallempudi on 2/3/16.
+//  Copyright © 2016 rajeevprasanna. All rights reserved.
+//
+
+#import "CalendarEventParser.h"
+
+@implementation CalendarEventParser
+
+@end
